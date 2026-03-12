0b558e93bd81b7e8d3e653b4a20cb4a71b047956
JavierGilAmado <javier6il4mado@gmail.com>
2026-03-12 12:28:01 +0100

    feat: initial prototype

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Usuarios existentes:");
+        System.out.println("ID: 1 - Nombre: Manuel - Apellido: Garcia - Edad: 35");
+        System.out.println("ID: 2 - Nombre: Ana - Apellido: Ruiz - Edad: 38");
+        System.out.println("ID: 3 - Nombre: Pablo - Apellido: Cid - Edad: 28");
     }
 }
