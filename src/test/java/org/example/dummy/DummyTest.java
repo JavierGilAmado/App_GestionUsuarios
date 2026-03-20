ebadec92db30f18107023ab65595509cc6aef0bd
JavierGilAmado <javier6il4mado@gmail.com>
2026-03-20 12:18:18 +0100

    test + ci: add dummy test & workflow dummyTest

---
+++ src/test/java/org/example/dummy/DummyTest.java
@@ -0,0 +1,12 @@
+package org.example.dummy;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DummyTest {
+    @Test
+    void simpleTest() {
+        // Check JUnit run
+        assertTrue(true);
+    }
+}
